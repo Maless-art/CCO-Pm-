ae6aee701327ee5000b6a88719e1752a103b33c0
Maless-art <franklinmjj1@gmail.com>
2026-09-07 10:56:34 -0500

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -30,26 +30,13 @@ import {
 } from
 "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 const firebaseConfig = {
-
-    apiKey: "AIzaSyB8OXd7xWd66cux_TkCGA8OGrHV1b6SMS0",
-
-    authDomain:
-        "control-de-cobros-operativos.firebaseapp.com",
-
-    projectId:
-        "control-de-cobros-operativos",
-
-    storageBucket:
-        "control-de-cobros-operativos.firebasestorage.app",
-
-    messagingSenderId:
-        "431214411305",
-
-    appId:
-        "1:431214411305:web:a9428c6e05e310e2bb2638"
-
+  apiKey: "AIzaSyB6-4cIM2s5sHGxkNfLvREEfVnbQtrd2jg",
+  authDomain: "cco-pma.firebaseapp.com",
+  projectId: "cco-pma",
+  storageBucket: "cco-pma.firebasestorage.app",
+  messagingSenderId: "258875434069",
+  appId: "1:258875434069:web:5cff50eb14e0b56c3320dc"
 };
-
 
 const firebaseApp = initializeApp(firebaseConfig);
 
